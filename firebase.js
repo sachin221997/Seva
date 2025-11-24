7dcfb982d11d85cc2500eee551e44da0293e1141
Sachin Kumar Yadav <117505249+sachin221997@users.noreply.github.com>
2025-11-24 21:39:33 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+<!-- Firebase V8 SDK -->
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
 
-const firebaseConfig = {
+<script>
+// Your Firebase Config
+var firebaseConfig = {
   apiKey: "AIzaSyBOcsJLpaNSVcFo-Xl6UFEtItcoyxRFzik",
   authDomain: "seva-6f191.firebaseapp.com",
   projectId: "seva-6f191",
-  storageBucket: "seva-6f191.firebasestorage.app",
+  storageBucket: "seva-6f191.appspot.com",
   messagingSenderId: "826093348416",
   appId: "1:826093348416:web:19fefe43b9b7cba09b21a5",
   measurementId: "G-78H32SRPCD"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-window.auth = auth;
+// Make services global
+const auth = firebase.auth();
+const db = firebase.firestore();
+</script>
